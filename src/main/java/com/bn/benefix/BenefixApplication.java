4e729d8e01b8df06353593dbe7087efe5afae38a
João Vitor Chaves Silva <chavesprogrammer@gmail.com>
2026-01-06 10:18:46 -0300

    starter

---
+++ src/main/java/com/bn/benefix/BenefixApplication.java
@@ -0,0 +1,13 @@
+package com.bn.benefix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BenefixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BenefixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bn/benefix/BenefixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bn.benefix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BenefixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
