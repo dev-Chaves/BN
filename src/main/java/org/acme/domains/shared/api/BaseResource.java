3473eeebd68787cd33989ed3bca87c8ca21de8d7
Joao Vitor <chavesprogrammer@gmail.com>
2026-03-13 16:54:37 -0300

    feature: adicionando interface base para controller

--- src/main/java/org/acme/domains/shared/api/BaseResource.java
+++ src/main/java/org/acme/domains/shared/api/BaseResource.java
@@ -1,4 +1,35 @@
 package org.acme.domains.shared.api;
 
+import io.smallrye.mutiny.Uni;
+import jakarta.ws.rs.core.Response;
+
 public interface BaseResource {
+
+    default <T> Uni<Response> toCreated(Uni<T> useCaseResult){
+
+        return useCaseResult.onItem()
+                .transform(result ->
+                        Response
+                                .status(Response.Status.CREATED)
+                                .entity(result)
+                                .build());
+    }
+
+    default <T> Uni<Response> toOk(Uni<T> useCaseResult){
+        return useCaseResult.onItem()
+                .transform(result ->
+                        Response
+                                .status(Response.Status.OK)
+                                .build());
+    }
+
+    default <T> Uni<Response> delete(Uni<T> useCaseResult){
+        return useCaseResult.onItem()
+                .transform(result ->
+                        Response.status(Response.Status.NO_CONTENT)
+                                .entity(result)
+                                .build());
+
+    }
+
 }
