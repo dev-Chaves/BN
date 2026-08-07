862b15f93accffcc5b1535622029219bc0493bb2
Joao Vitor <chavesprogrammer@gmail.com>
2026-08-07 09:16:18 -0300

    chore: initialize spring backend

---
+++ src/test/java/com/bnfix/ubm/UbmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bnfix.ubm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UbmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
